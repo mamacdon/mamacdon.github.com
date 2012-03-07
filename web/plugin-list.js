0a636339c8f41a587d43aa5fbe66afb80b062a98
Mark Macdonald <mamacdon@ca.ibm.com>
2012-03-07 13:16:33 -0500

    Remove pre-0.2 plugins from list. Add 0.5 to list

--- web/plugin-list.js
+++ web/plugin-list.js
@@ -7,86 +7,43 @@ define(['orion/URITemplate', 'domReady!'], function(URITemplate, document) {
 		{	name: "JS Beautify",
 			description: 'Cleans up the formatting of your JavaScript code using <a href="http://jsbeautifier.org/">jsbeautifier</a>.',
 			versions: {
-				/*"0.2 M6/M7": [
-					"/m6/beautify/jsbeautify.html",
-					"https://github.com/mamacdon/mamacdon.github.com/tree/master/m6/beautify"
-				],*/
-				"0.2": [
-					"/0.2/plugins/beautify/jsbeautify.html",
-					"https://github.com/mamacdon/mamacdon.github.com/tree/master/m6/beautify"
-				],
-				"0.3": [
-					"/0.2/plugins/beautify/jsbeautify.html",
-					"https://github.com/mamacdon/mamacdon.github.com/tree/master/m6/beautify"
-				],
-				"0.4": [
-					"/0.2/plugins/beautify/jsbeautify.html",
-					"https://github.com/mamacdon/mamacdon.github.com/tree/master/m6/beautify"
-				]
+				"0.2": ["/0.2/plugins/beautify/jsbeautify.html", "https://github.com/mamacdon/mamacdon.github.com/tree/master/m6/beautify"],
+				"0.3": ["/0.2/plugins/beautify/jsbeautify.html", "https://github.com/mamacdon/mamacdon.github.com/tree/master/m6/beautify"],
+				"0.4": ["/0.2/plugins/beautify/jsbeautify.html", "https://github.com/mamacdon/mamacdon.github.com/tree/master/m6/beautify"],
+				"0.5": ["/0.2/plugins/beautify/jsbeautify.html", "https://github.com/mamacdon/mamacdon.github.com/tree/master/m6/beautify"]
 			}
 		},
 		{	name: "Uglify",
 			description: 'Minifies your JavaScript code using <a href="https://github.com/mishoo/UglifyJS">uglify-js</a>.',
 			versions: {
-				/*"0.2 M6/M7": [
-					"/m6/uglify/uglify-plugin.html",
-					"https://github.com/mamacdon/mamacdon.github.com/tree/master/m6/uglify"
-				],*/
-				"0.2": [
-					"/0.2/plugins/uglify/uglify-plugin.html",
-					"https://github.com/mamacdon/mamacdon.github.com/tree/master/0.2/plugins/uglify"
-				],
-				"0.3": [
-					"/0.2/plugins/uglify/uglify-plugin.html",
-					"https://github.com/mamacdon/mamacdon.github.com/tree/master/0.2/plugins/uglify"
-				],
-				"0.4": [
-					"/0.2/plugins/uglify/uglify-plugin.html",
-					"https://github.com/mamacdon/mamacdon.github.com/tree/master/0.2/plugins/uglify"
-				]
+				/*"0.2 M6/M7": ["/m6/uglify/uglify-plugin.html", "https://github.com/mamacdon/mamacdon.github.com/tree/master/m6/uglify"],*/
+				"0.2": ["/0.2/plugins/uglify/uglify-plugin.html", "https://github.com/mamacdon/mamacdon.github.com/tree/master/0.2/plugins/uglify"],
+				"0.3": ["/0.2/plugins/uglify/uglify-plugin.html", "https://github.com/mamacdon/mamacdon.github.com/tree/master/0.2/plugins/uglify"],
+				"0.4": ["/0.2/plugins/uglify/uglify-plugin.html", "https://github.com/mamacdon/mamacdon.github.com/tree/master/0.2/plugins/uglify"],
+				"0.5": ["/0.2/plugins/uglify/uglify-plugin.html", "https://github.com/mamacdon/mamacdon.github.com/tree/master/0.2/plugins/uglify"]
 			}
 		},
 		{	name: "CSSLint",
 			description: 'Provides validation and style tips on your CSS files using <a href="https://github.com/stubbornella/csslint">CSS Lint</a>. (Included with Orion by default starting in 0.4)',
 			versions: {
-				"0.2": [
-					"/0.2/plugins/csslint/csslintPlugin.html",
-					"https://github.com/mamacdon/mamacdon.github.com/tree/master/0.2/plugins/csslint"
-				],
-				"0.3": [
-					"/0.3/plugins/csslint/csslintPlugin.html",
-					"https://github.com/mamacdon/mamacdon.github.com/tree/master/0.3/plugins/csslint"
-				]
+				"0.2": ["/0.2/plugins/csslint/csslintPlugin.html", "https://github.com/mamacdon/mamacdon.github.com/tree/master/0.2/plugins/csslint"],
+				"0.3": ["/0.3/plugins/csslint/csslintPlugin.html", "https://github.com/mamacdon/mamacdon.github.com/tree/master/0.3/plugins/csslint"]
 			}
 		},
 		{	name: "reverse",
 			description: 'A minimal plugin example used as part of a <a href="http://wiki.eclipse.org/Orion/Documentation/Developer_Guide/Simple_plugin_example">tutorial</a>.',
 			versions: {
-				/*"0.2 M6/M7": [
-					"/m6/uglify/uglify-plugin.html",
-					"https://github.com/mamacdon/mamacdon.github.com/tree/master/m6/reverse"
-				],*/
-				"0.2": [
-					"/0.2/plugins/reverse/reversePlugin.html",
-					"https://github.com/mamacdon/mamacdon.github.com/tree/master/0.2/plugins/reverse"
-				],
-				"0.3": [
-					"/0.2/plugins/reverse/reversePlugin.html",
-					"https://github.com/mamacdon/mamacdon.github.com/tree/master/0.2/plugins/reverse"
-				]
+				/*"0.2 M6/M7": ["/m6/uglify/uglify-plugin.html", "https://github.com/mamacdon/mamacdon.github.com/tree/master/m6/reverse"],*/
+				"0.2": ["/0.2/plugins/reverse/reversePlugin.html", "https://github.com/mamacdon/mamacdon.github.com/tree/master/0.2/plugins/reverse"],
+				"0.3": ["/0.2/plugins/reverse/reversePlugin.html", "https://github.com/mamacdon/mamacdon.github.com/tree/master/0.2/plugins/reverse"]
 			}
 		},
 		{	name: "Bugzilla",
 			description: 'A <a href="http://dev.eclipse.org/mhonarc/lists/orion-dev/msg00688.html">Bugzilla integration</a> plugin for Orion.',
 			versions: {
-				"0.3": [
-					"/0.3/plugins/bugzilla/plugin.html",
-					"https://github.com/mamacdon/mamacdon.github.com/tree/master/0.3/plugins/bugzilla"
-				],
-				"0.4": [
-					"/0.3/plugins/bugzilla/plugin.html",
-					"https://github.com/mamacdon/mamacdon.github.com/tree/master/0.3/plugins/bugzilla"
-				]
+				"0.3": ["/0.3/plugins/bugzilla/plugin.html", "https://github.com/mamacdon/mamacdon.github.com/tree/master/0.3/plugins/bugzilla"],
+				"0.4": ["/0.3/plugins/bugzilla/plugin.html", "https://github.com/mamacdon/mamacdon.github.com/tree/master/0.3/plugins/bugzilla"],
+				"0.5": ["/0.3/plugins/bugzilla/plugin.html", "https://github.com/mamacdon/mamacdon.github.com/tree/master/0.3/plugins/bugzilla"]
 			}
 		},
 		//{	name: "language.js",
@@ -107,66 +64,48 @@ define(['orion/URITemplate', 'domReady!'], function(URITemplate, document) {
 			description: 'Provides a simple tree view of the functions in a JavaScript file.',
 			experimental: true,
 			versions: {
-				"0.3": [
-					"http://mamacdon.github.com/outliner/outlinerPlugin.html",
-					"https://github.com/mamacdon/outliner"
-				],
-				"0.4": [
-					"http://mamacdon.github.com/outliner/outlinerPlugin.html",
-					"https://github.com/mamacdon/outliner"
-				]
+				"0.3": ["http://mamacdon.github.com/outliner/outlinerPlugin.html", "https://github.com/mamacdon/outliner"],
+				"0.4": ["http://mamacdon.github.com/outliner/outlinerPlugin.html", "https://github.com/mamacdon/outliner"],
+				"0.5": ["http://mamacdon.github.com/outliner/outlinerPlugin.html", "https://github.com/mamacdon/outliner"]
 			}
 		},
 		{	name: "Nonymous outline",
 			description: 'Provides a tree view of JavaScript functions using the \"Function-Object Consumption\" algorithm to generate meaningful names for anonymous functions.',
 			experimental: true,
 			versions: {
-				"0.3": [
-					"http://johnjbarton.github.com/outliner/nonymousPlugin.html",
-					"https://github.com/johnjbarton/outliner"
-				],
-				"0.4": [
-					"http://johnjbarton.github.com/outliner/nonymousPlugin.html",
-					"https://github.com/johnjbarton/outliner"
-				]
+				"0.3": ["http://johnjbarton.github.com/outliner/nonymousPlugin.html", "https://github.com/johnjbarton/outliner"],
+				"0.4": ["http://johnjbarton.github.com/outliner/nonymousPlugin.html", "https://github.com/johnjbarton/outliner"],
+				"0.5": ["http://johnjbarton.github.com/outliner/nonymousPlugin.html", "https://github.com/johnjbarton/outliner"]
 			}
 		},
 		{	name: "CodeMirror highlighting",
 			description: 'Uses <a href="http://codemirror.net/">CodeMirror</a> modes to highlight your code.',
 			experimental: true,
 			versions: {
-				"0.4": [
-					"http://mamacdon.github.com/orion-codemirror/codeMirrorPlugin.html",
-					"https://github.com/mamacdon/orion-codemirror"
-				]
+				"0.4": ["http://mamacdon.github.com/orion-codemirror/codeMirrorPlugin.html", "https://github.com/mamacdon/orion-codemirror"],
+				"0.5": ["http://mamacdon.github.com/orion-codemirror/codeMirrorPlugin.html", "https://github.com/mamacdon/orion-codemirror"]
 			}
 		},
 		{	name: "HTML Outline",
 			description: 'Provides a hierarchical outline of the elements in an HTML file.',
 			versions: {
-				"0.4": [
-					"http://jarthorn.github.com/html-tools/htmlOutlinePlugin.html",
-					"https://github.com/jarthorn/html-tools"
-				]
+				"0.4": ["http://jarthorn.github.com/html-tools/htmlOutlinePlugin.html", "https://github.com/jarthorn/html-tools"],
+				"0.5": ["http://jarthorn.github.com/html-tools/htmlOutlinePlugin.html", "https://github.com/jarthorn/html-tools"]
 			}
 		},
 		{	name: "Esprima outline",
 			description: 'Uses the <a href="http://esprima.org">esprima</a> JavaScript parser to provide a detailed outline of your JavaScript code.',
 			versions: {
-				"0.4": [
-					"http://aclement.github.com/esprima-outline/esprimaOutlinerPlugin.html",
-					"https://github.com/aclement/esprima-outline"
-				]
+				"0.4": ["http://aclement.github.com/esprima-outline/esprimaOutlinerPlugin.html", "https://github.com/aclement/esprima-outline"],
+				"0.5": ["http://aclement.github.com/esprima-outline/esprimaOutlinerPlugin.html", "https://github.com/aclement/esprima-outline"]
 			}
 		},
 		{
 			name: "Esprima content assist",
 			description: "Offers completion and type inference for JavaScript files.",
 			versions: {
-				"0.4": [
-						"http://aeisenberg.github.com/esprimaContentAssist/esprimaJsContentAssistPlugin.html",
-						"https://github.com/aeisenberg/esprimaContentAssist"
-				]
+				"0.4": ["http://aeisenberg.github.com/esprimaContentAssist/esprimaJsContentAssistPlugin.html", "https://github.com/aeisenberg/esprimaContentAssist"],
+				"0.5": ["http://aeisenberg.github.com/esprimaContentAssist/esprimaJsContentAssistPlugin.html", "https://github.com/aeisenberg/esprimaContentAssist"]
 			}
 		}
 		];
