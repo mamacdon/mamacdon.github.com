f5f8934f1a1c5d9c2a5e0c19bedf09851afbd33f
Mark Macdonald <Mark_MacDonald@ca.ibm.com>
2013-08-08 15:47:57 -0400

    Clean up plugin version ranges, nicer table/CSS

--- web/plugin-list.js
+++ web/plugin-list.js
@@ -6,56 +6,53 @@ define(['orion/URITemplate', 'domReady!'], function(URITemplate, document) {
 		{	name: 'JS Beautify',
 			description: 'Cleans up the formatting of your JavaScript code using <a href="http://jsbeautifier.org/">jsbeautifier</a>.',
 			versions: {
-				'0.5': ['/0.5/plugins/beautify/jsbeautify.html', 'https://github.com/mamacdon/mamacdon.github.com/tree/master/0.5/beautify'],
-				'1.0|2.0': ['/0.5/plugins/beautify/jsbeautify.html', 'https://github.com/mamacdon/mamacdon.github.com/tree/master/0.5/beautify']
+				'>=1.0': ['/0.5/plugins/beautify/jsbeautify.html', 'https://github.com/mamacdon/mamacdon.github.com/tree/master/0.5/beautify']
 			}
 		},
 		{	name: 'Uglify',
 			description: 'Minifies your JavaScript code using <a href="https://github.com/mishoo/UglifyJS">uglify-js</a>.',
 			versions: {
 				/*"0.2 M6/M7": ["/m6/uglify/uglify-plugin.html", "https://github.com/mamacdon/mamacdon.github.com/tree/master/m6/uglify"],*/
-				'0.5|2.0': ['/0.5/plugins/uglify/uglify-plugin.html', 'https://github.com/mamacdon/mamacdon.github.com/tree/master/0.5/plugins/uglify']
+				'>=0.5': ['/0.5/plugins/uglify/uglify-plugin.html', 'https://github.com/mamacdon/mamacdon.github.com/tree/master/0.5/plugins/uglify']
 			}
 		},
 		{	name: 'Bugzilla',
 			description: 'A <a href="http://dev.eclipse.org/mhonarc/lists/orion-dev/msg00688.html">Bugzilla integration</a> plugin for Orion.',
 			versions: {
-				'0.5': ['/0.3/plugins/bugzilla/plugin.html', 'https://github.com/mamacdon/mamacdon.github.com/tree/master/0.3/plugins/bugzilla']
+				'>=0.5': ['/0.3/plugins/bugzilla/plugin.html', 'https://github.com/mamacdon/mamacdon.github.com/tree/master/0.3/plugins/bugzilla']
 			}
 		},
 		{	name: 'JavaScript outliner',
 			description: 'Provides a simple tree view of the functions in a JavaScript file.',
 			experimental: true,
 			versions: {
-				'0.5': ['/0.5/plugins/outliner/outlinerPlugin.html', 'https://github.com/mamacdon/outliner/tree/v0.2'],
-				'1.0|2.0': ['http://mamacdon.github.io/outliner/outlinerPlugin.html', 'https://github.com/mamacdon/outliner']
+				'>=1.0': ['http://mamacdon.github.io/outliner/outlinerPlugin.html', 'https://github.com/mamacdon/outliner']
 			}
 		},
 		{	name: 'Nonymous outline',
 			description: 'Provides a tree view of JavaScript functions using the \"Function-Object Consumption\" algorithm to generate meaningful names for anonymous functions.',
 			experimental: true,
 			versions: {
-				'0.5|1.0|2.0': ['http://johnjbarton.github.io/outliner/nonymousPlugin.html', 'https://github.com/johnjbarton/outliner']
+				'>=0.5': ['http://johnjbarton.github.io/outliner/nonymousPlugin.html', 'https://github.com/johnjbarton/outliner']
 			}
 		},
 		{	name: 'CodeMirror',
 			description: 'Uses <a href="http://codemirror.net/">CodeMirror</a> modes to highlight your code.',
 			experimental: true,
 			versions: {
-				'0.5': ['/0.5/plugins/orion-codemirror/codeMirrorPlugin.html', 'https://github.com/mamacdon/orion-codemirror/tree/v0.1.9'],
 				'1.0|2.0': ['http://mamacdon.github.io/orion-codemirror/codeMirrorPlugin.html', 'https://github.com/mamacdon/orion-codemirror']
 			}
 		},
 		{	name: 'HTML Outline',
 			description: 'Provides a hierarchical outline of the elements in an HTML file.',
 			versions: {
-				'0.5': ['http://jarthorn.github.io/html-tools/htmlOutlinePlugin.html', 'https://github.com/jarthorn/html-tools']
+				'>=0.5': ['http://jarthorn.github.io/html-tools/htmlOutlinePlugin.html', 'https://github.com/jarthorn/html-tools']
 			}
 		},
 		{	name: 'Esprima outline',
 			description: 'Uses the <a href="http://esprima.org">esprima</a> JavaScript parser to provide a detailed outline of your JavaScript code.',
 			versions: {
-				'0.5|1.0|2.0': ['http://aclement.github.io/esprima-outline/esprimaOutlinerPlugin.html', 'https://github.com/aclement/esprima-outline']
+				'>=0.5': ['http://aclement.github.io/esprima-outline/esprimaOutlinerPlugin.html', 'https://github.com/aclement/esprima-outline']
 			}
 		},
 		{
@@ -69,61 +66,61 @@ define(['orion/URITemplate', 'domReady!'], function(URITemplate, document) {
 			description: 'Enables you to use Orion to work with files and folders in an <a href="http://dev.w3.org/2009/dap/file-system/pub/FileSystem/">HTML5 file system</a> stored on your local computer.',
 			orionHome: true,
 			versions: {
-				'1.0|2.0': ['{OrionHome}/plugins/HTML5LocalFilePlugin.html', 'https://github.com/eclipse/orion.client/blob/master/bundles/org.eclipse.orion.client.core/web/plugins/HTML5LocalFilePlugin.html']
+				'>=1.0': ['{OrionHome}/plugins/HTML5LocalFilePlugin.html', 'https://github.com/eclipse/orion.client/blob/master/bundles/org.eclipse.orion.client.core/web/plugins/HTML5LocalFilePlugin.html']
 			}
 		},
 		{	name: 'Amazon S3 filesystem',
 			description: 'Provides access to an Amazon S3 bucket as a filesystem. See <a href=\'https://github.com/mamacdon/orion-s3/blob/master/README.md\'>README</a> for installation instructions.',
 			versions: {
-				'0.5': [null, 'https://github.com/mamacdon/orion-s3/']
+				'>=0.5': [null, 'https://github.com/mamacdon/orion-s3/']
 			}
 		},
 		{	name: 'ToRGB',
 			description: 'Converts an <code>r,g,b</code> decimal to hex #RRGGBB for use in CSS.',
 			orionHome: true, // hosted in your Orion, not the web
 			versions: {
-				'0.5': ['{OrionHome}/plugins/toRGBPlugin.html', 'https://github.com/eclipse/orion.client/blob/master/bundles/org.eclipse.orion.client.core/web/plugins/toRGBPlugin.html']
+				'>=0.5': ['{OrionHome}/plugins/toRGBPlugin.html', 'https://github.com/eclipse/orion.client/blob/master/bundles/org.eclipse.orion.client.core/web/plugins/toRGBPlugin.html']
 			}
 		},
 		{	name: 'Pixlr',
 			description: 'Provides integration with <a href=\'http://www.pixlr.com/\'>Pixlr</a>, the online image editor. Allows Pixlr to open images from your Orion workspace, and save files back to Orion.',
 			versions: {
-				'0.5|1.0|2.0': ['http://sfmccourt.github.io/plugins/pixlr/pixlrPlugin.html', 'https://github.com/sfmccourt/sfmccourt.github.com/blob/master/plugins/pixlr/pixlrPlugin.html']
+				'>=0.5': ['http://sfmccourt.github.io/plugins/pixlr/pixlrPlugin.html', 'https://github.com/sfmccourt/sfmccourt.github.com/blob/master/plugins/pixlr/pixlrPlugin.html']
 			}
 		},
 		{	name: 'String Externalizer',
 			description: 'Helps you externalize strings in your JavaScript code. <a href=\'http://dev.eclipse.org/mhonarc/lists/orion-dev/msg01710.html\'>Read more here</a>.',
 			orionHome: true,
 			versions: {
-				'0.5|1.0|2.0': ['{OrionHome}/plugins/nonnlsPlugin.html', 'https://github.com/eclipse/orion.client/blob/master/bundles/org.eclipse.orion.client.core/web/plugins/nonnlsPlugin.html']
+				'>=0.5': ['{OrionHome}/plugins/nonnlsPlugin.html', 'https://github.com/eclipse/orion.client/blob/master/bundles/org.eclipse.orion.client.core/web/plugins/nonnlsPlugin.html']
 			}
 		},
 		{	name: 'Projects Prototype',
 			description: 'Enable the prototype of supporting Projects within Orion. Adds a banner item for Projects',
 			orionHome: true,
 			versions: {
-				'2.0': ['{OrionHome}/plugins/projectsPlugin.html', 'https://github.com/eclipse/orion.client/blob/master/bundles/org.eclipse.orion.client.ui/web/plugins/projectsPlugin.html']
+				'>=2.0': ['{OrionHome}/plugins/projectsPlugin.html', 'https://github.com/eclipse/orion.client/blob/master/bundles/org.eclipse.orion.client.ui/web/plugins/projectsPlugin.html']
 			}
 		},
 		{	name: 'Snippet',
 			description: 'Editor command to create an HTML code snippet from a selection in your editor. <a href=\'http://kenwalker.github.io/snippet/Snippet.html\'>Read more here</a>.',
 			orionHome: false,
 			versions: {
-				'2.0': ['http://kenwalker.github.io/snippet/Snippet.html', 'https://github.com/kenwalker/snippet']
+				'>=2.0': ['http://kenwalker.github.io/snippet/Snippet.html', 'https://github.com/kenwalker/snippet']
 			}
 		},
 		{	name: 'Double2Single',
 			description: 'Editor command to convert double quotes to single. <a href=\'http://kenwalker.github.io/Double2Single/Double2Single.html\'>Read more here</a>.',
 			orionHome: false,
 			versions: {
-				'2.0': ['http://kenwalker.github.io/Double2Single/Double2Single.html', 'https://github.com/kenwalker/Double2Single']
+				'>=2.0': ['http://kenwalker.github.io/Double2Single/Double2Single.html', 'https://github.com/kenwalker/Double2Single']
 			}
 		},
 		{	name: 'Line Delimiter Converter',
 			description: 'This plugin provides a editor commands to convert line delimiters to DOS or UNIX.',
 			orionHome: true,
 			versions: {
-				'0.5|1.0|2.0': ['{OrionHome}/plugins/delimiterPlugin.html', 'https://github.com/eclipse/orion.client/blob/master/bundles/org.eclipse.orion.client.ui/web/plugins/delimiterPlugin.js']
+				'>2.0': ['{OrionHome}/plugins/delimiterPlugin.html', 'https://github.com/eclipse/orion.client/blob/master/bundles/org.eclipse.orion.client.ui/web/plugins/delimiterPlugin.js']
 			}
 		}
 		// TODO unitTestplugin (when it works)
@@ -163,25 +160,51 @@ define(['orion/URITemplate', 'domReady!'], function(URITemplate, document) {
 				},
 				getName: function() { return this.name; },
 				getVersion: function(versionId) {
-					var keys = Object.keys(this.versions);
-					for (var i=0; i < keys.length; i++) {
-						var ver = keys[i];
-						if (ver.split('|').indexOf(versionId) !== -1) {
-							return this.versions[ver];
-						}
+					var versions = this.versions,
+					    keys = Object.keys(versions);
+					// Try an exact match first
+					if (Object.prototype.hasOwnProperty.call(versions, versionId)) {
+						return versions[versionId];
 					}
-					return null;
+					// Otherwise check for a range
+					var version;
+					keys.some(function(key) {
+						var regexMatch = /([<>]=?)([\d.]+)/.exec(key),
+						    rangeMatch;
+						if (regexMatch) {
+							var op = regexMatch[1], number = parseFloat(regexMatch[2]);
+							switch (op) {
+							case "<":
+								rangeMatch = (versionId < number);
+								break;
+							case ">": 
+								rangeMatch = (versionId > number);
+								break;
+							case "<=":
+								rangeMatch = (versionId <= number);
+								break;
+							case ">=": 
+								rangeMatch = (versionId >= number);
+								break;
+							}
+							if (rangeMatch) {
+								version = versions[key];
+								return true;
+							}
+						}
+						return false;
+					});
+					return version || null;
+//					for (var i=0; i < keys.length; i++) {
+//						var ver = keys[i];
+//						if (ver.split('|').indexOf(versionId) !== -1) {
+//							return versions[ver];
+//						}
+//					}
+//					return null;
 				},
 				getVersionIds: function() {
-					var versions = Object.keys(this.versions);
-					var keys = [];
-					for (var i=0; i < versions.length; i++) {
-						var vs = versions[i].split('|');
-						for (var j=0; j < vs.length; j++) {
-							keys.push(vs[j]);
-						}
-					}
-					return keys;
+					return Object.keys(this.versions);
 				}
 			};
 			for (var i = 0; i < pluginsData.length; i++) {
@@ -189,24 +212,10 @@ define(['orion/URITemplate', 'domReady!'], function(URITemplate, document) {
 			}
 		}());
 		function getPluginsMatching(predicate) {
-			var plugins = [];
-			for (var i=0; i < pluginsData.length; i++) {
-				var plugin = pluginsData[i];
-				if (predicate(plugin)) {
-					plugins.push(plugin);
-				}
-			}
-			return plugins;
+			return pluginsData.filter(predicate);
 		}
 		function getAllVersions(/**Plugin[]*/ plugins) {
-			var versions = {};
-			for (var i=0; i < plugins.length; i++) {
-				var versionIds = plugins[i].getVersionIds();
-				for (var j=0; j < versionIds.length; j++) {
-					versions[versionIds[j]] = null;
-				}
-			}
-			return Object.keys(versions);
+			return '0.5 1.0 2.0 3.0 4.0'.split(' ');
 		}
 		function parseParameters() {
 			var params = {};
@@ -225,21 +234,31 @@ define(['orion/URITemplate', 'domReady!'], function(URITemplate, document) {
 		}
 		function createTbody(table, plugins, params) {
 			function createHeaderRow(table, versions) {
-				var headerRow = table.insertRow(-1);
+				// Plugin header cell
+				var headerRow1 = table.insertRow(-1);
 				var th = document.createElement('th');
-				th.innerHTML = '';
-				headerRow.appendChild(th);
-				if (params[VERSION]) {
-					th = document.createElement('th');
-					th.innerHTML = '';
-					headerRow.appendChild(th);
-				} else {
-					for (var i=0; i < versions.length; i++) {
-						th = document.createElement('th');
-						th.innerHTML = 'Orion ' + versions[i];
-						headerRow.appendChild(th);
-					}
+				th.textContent = "Plugin";
+				th.setAttribute('rowspan', 2);
+				headerRow1.appendChild(th);
+
+				// Orion Version header cell
+				th = document.createElement('th');
+				th.textContent = "Orion Version";
+				th.setAttribute('colspan', versions.length);
+				headerRow1.appendChild(th);
+
+				// Subheaders for each version (or if installing into a target Orion, a single
+				// column for the target's version)
+				var headerRow2 = table.insertRow(-1);
+				if (Object.prototype.hasOwnProperty.call(params, VERSION)) {
+					versions = [params[VERSION]];
 				}
+
+				versions.forEach(function(version) {
+					th = document.createElement('th');
+					th.innerHTML = '' + version;
+					headerRow2.appendChild(th);
+				});
 			}
 			function getQualifiedInstallURL(target, /**Plugin*/ plugin, versionId, /*String?*/ orionHome) {
 				function qualify(url) {
